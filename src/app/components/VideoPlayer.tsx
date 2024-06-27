3bf4668ef753c2a7bcbf7541f196758186a43f6b
Ronald <vargasron4@gmail.com>
2024-06-27 10:07:18 -0700

    switch video player package
    switch to react-player package that allows embeds

--- src/app/components/VideoPlayer.tsx
+++ src/app/components/VideoPlayer.tsx
@@ -1,21 +1,23 @@
 import "video-react/dist/video-react.css";
 import "./customVideoStyles.css";
 import { Box, createStyles } from "@mantine/core";
-import React, { memo } from "react";
-import {
-  Player,
-  ControlBar,
-  BigPlayButton,
-  PlaybackRateMenuButton,
-  VolumeMenuButton,
-} from "video-react";
+import React, { memo, useState } from "react";
+import ReactPlayer from "react-player";
 
 const useStyles = createStyles(() => ({
-  videoPlayer: {
+  videoContainer: {
     width: "100%",
     height: "100%",
     position: "relative",
     borderRadius: 12,
+    paddingTop: '56.25%', // 16:9 aspect ratio
+  },
+  videoPlayer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
   },
   overlay: {
     position: "absolute",
@@ -26,7 +28,7 @@ const useStyles = createStyles(() => ({
     zIndex: 2,
     backgroundColor: "transparent",
     cursor: "pointer",
-  }
+  },
 }));
 
 type VideoPlayerProps = {
@@ -35,16 +37,25 @@ type VideoPlayerProps = {
 };
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, fullPlayer }) => {
   const { classes } = useStyles();
+  const [isPlaying, setIsPlaying] = useState(fullPlayer);
+  const [volume, setVolume] = useState(0.5);
+
+  const onPlayClick = () => {
+    setIsPlaying(!isPlaying);
+  };
 
   return (
-    <Box className={classes.videoPlayer}>
-      <Player src={src} autoPlay={fullPlayer}>
-        <BigPlayButton position={fullPlayer ? "center" : "left"} />
-        {fullPlayer && <ControlBar>
-          <PlaybackRateMenuButton rates={[2, 1.5, 1, 0.5]} />
-          <VolumeMenuButton vertical />
-        </ControlBar>}
-      </Player>
+    <Box className={classes.videoContainer}>
+      <ReactPlayer
+        className={classes.videoPlayer}
+        url={src}
+        controls={fullPlayer}
+        playing={isPlaying}
+        volume={volume}
+        width="100%"
+        height="100%"
+        pip={false}
+      />
       {!fullPlayer && <Box className={classes.overlay} />}
     </Box>
   );
